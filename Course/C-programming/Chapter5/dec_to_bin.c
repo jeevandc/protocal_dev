f38c8b8f0720e90a9753db26fb0afc0357eb9480
jeevan <jeevan.9571@gmail.com>
2021-11-07 22:37:51 +0530

    Program-3

---
+++ Course/C-programming/Chapter5/dec_to_bin.c
@@ -0,0 +1,27 @@
+// convert decimal to binary
+
+#include <stdio.h>
+
+
+int main() {
+  int n, dec=0;
+  long int bin = 0;
+  int rem, i = 1;
+  printf("Enter a decimal number: ");
+  scanf("%d", &n);
+  dec=n;
+//  bin = convert(n);
+  printf("\nReverse Order:");
+  while (n!=0) {
+    rem = n % 2;
+    n /= 2;
+    bin += rem * i;
+    i *= 10;
+    printf("%d",rem);
+  //  printf("2|%d-%d \n __\n",n,rem);
+  }
+  printf("\n*******************************\n");
+  printf("%d in decimal =  %ld in binary", dec, bin);
+  printf("\n*******************************\n");
+  return 0;
+}
