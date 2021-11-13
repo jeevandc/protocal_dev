b31d0601d4b700ff28e1b61a72208af6979706ca
jeevan <jeevan.9571@gmail.com>
2021-11-13 10:25:27 +0530

    Program5

---
+++ Course/C-programming/Chapter5/prime_add.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int i=0,count=0,j=0;
+	int input=0,result=0;
+	printf("Enter Number : \n");
+	scanf("%d",&input);
+	
+		for(j=2;j<=input;j++)
+		{
+			count=0;
+			printf("j=%d\n",j);
+			for(i=2;i<j;i++)
+			{
+				if(j%i==0)
+				count++;
+
+			}
+			if(count==0)
+			{
+       				 result+=j;
+				printf("Result=%d\n",result);
+			}
+		
+		
+		}
+		printf("Result:%d\n",result);
+
+return 0;
+
+}
+
